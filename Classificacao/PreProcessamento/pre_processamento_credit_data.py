81a110de14be403fe19ebd1617a2cdf02b914500
Victor Mello <victorsmello93@gmail.com>
2020-01-18 16:50:33 -0300

    Implementação do método read_csv para ler o arquivo csv - Capítulo de classificação - Pré-processamento

--- Classificacao/PreProcessamento/pre_processamento_credit_data.py
+++ Classificacao/PreProcessamento/pre_processamento_credit_data.py
@@ -4,4 +4,10 @@ Created on Sat Jan 18 16:36:23 2020
 
 @author: santo
 """
+import pandas as pd
+base = pd.read_csv('credit_data.csv')
+
+#exibindo os valores estatísticos da base de dados do arquivo csv 
+#(quantidade, média, desvio padrão, quartis, etc...)
+base.describe()
 
